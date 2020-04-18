ec3a3853ad4e61c3c5dc3a33529642c43c3fd052
joão marcos maracaipe <maracaipeads@gmail.com>
2020-04-18 17:30:04 -0300

    criando minha primeira rota com formato de resposta json

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'João Marcos Maracaipe'
+    });
+
+});
+
 app.listen(3333);
